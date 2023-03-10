0e723e0ddf9a1fe0b0e400a51cedd27fed82defe
Vladislav <v.genkel@consult-info.ru>
2023-03-10 10:16:09 +0100

    перенес в отдельный файл код из init

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,17 +3,11 @@
 require_once __DIR__ . '/include/autoload.php';
 require_once __DIR__ . '/include/settings.php';
 
+include 'events.php';
+
 AddEventHandler('iblock', 'OnAfterIBlockElementAdd', ['\Blagopar\Events\Iblock', 'onAfterIBlockElementAddHandler']);
 
-function custom_mail($to,$subject,$body,$headers) {
-    $f=fopen($_SERVER["DOCUMENT_ROOT"]."/maillog.txt", "a+");
-    fwrite($f, print_r(array('TO' => $to, 'SUBJECT' => $subject, 'BODY' => $body, 'HEADERS' => $headers),1)."\n========\n");
-    fclose($f);
-    return mail($to,$subject,$body,$headers);
-}
+$eventManager = \Bitrix\Main\EventManager::getInstance();
+$eventManager->addEventHandler('sale', 'OnSaleOrderSaved', 'rsOnAddOrder');
 
-AddEventHandler('sale', 'OnOrderNewSendEmail', ['\Blagopar\Events\Iblock', 'modifyNewOrderEmailFields']);
-
-
-if (file_exists($_SERVER["DOCUMENT_ROOT"]."/php_interface/include/functions.php"))
-    require_once($_SERVER["DOCUMENT_ROOT"]."/php_interface/include/functions.php");
+AddEventHandler('sale', 'OnOrderNewSendEmail', 'modifyNewOrderEmailFields');
